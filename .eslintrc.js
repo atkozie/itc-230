c6b465180f83a8f91952eae6e26bba01fecf239f
Adam Kozie <atkozie@gmail.com>
2017-05-01 22:09:17 -0700

    HW Week 4 - Quality Matters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "standard",
-        "promise"
-    ]
+    "env": {
+    "es6": true,
+    "node": true
+    },
+    "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+    "prefer-arrow-callback": "warn",
+    "no-var": "warn",
+    }//end rules
 };
---
+++ hw4.js
@@ -0,0 +1,7 @@
+// eslint practice file for ITC 230
+
+let names = ['sara','joe','dave','ann'];
+
+names.map = function(item) {
+    return item.toUpperCase();
+}
