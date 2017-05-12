8d155ca6c8a805d0d81466309a6ccc91bfa85af6
Adam Kozie <atkozie@gmail.com>
2017-05-11 20:46:50 -0700

    progress on templating

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict'
+//this is the server file
 
 const express = require("express");
 const app = express();
@@ -6,35 +7,41 @@ const handlebars = require('express-handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-const book = require('./lib/book.js'); //and we need the array from the other file
+const book = require('./lib/book.js'); // we need the array from the other file
 
 app.use(express.static(__dirname + '/public')); //path for static pages
 app.use(require('body-parser').urlencoded({extended: true})); //this parses the POSTed form submissions
-
-//Express can use a 'view' to render dynamic information that differs with each request. 
-//You can specify that views use a file extension other than 'html' if desired.
-app.engine(".html", handlebars({extname: '.html'}));
+ 
+//You can specify here that views use a file extension other than 'html' if desired.
+app.engine(".html", handlebars({extname: '.html', defaultLayout: 'main'}));
 app.set("view engine", ".html");
+
 
 //the pages
 //Route handlers are specified with app.get() or app.post(), & error handlers w/ app.use():
 //home page is sent as static file - .html file is in public folder
 app.get('/', function(req,res){
     res.type('text/html');
-    res.sendFile(__dirname + '/public/home.html'); 
+//    res.sendFile(__dirname + '/public/home.html'); //old code from pre-templating
+    res.render('home', {books: book.getAll()}); //passing the getAll from book, as books
+});
+
+app.get('/list', function(req,res){
+    res.type('text/html');
+//    res.sendFile(__dirname + '/public/home.html'); //old code from pre-templating
+    res.render('list', {books: book.getAll()}); //passing the getAll from book, as books
 });
 
 //about page is sent as a plain text response
 app.get('/about', function(req,res){
     res.type('text/plain');
-    res.send('About page');
+    res.send('This is the About page. It was rendered entirely in the server file');
 });
 
 //POST - using body-parser plugin
 app.post('/get', function(req,res){//URL will have /get? followed by querystring
     let gotten = book.get(req.body.title);
     res.render("details", {gotten: gotten});
-    //console.log(req.body); // display parsed form submission
 });
 
 // handle deletion with a GET
@@ -58,8 +65,7 @@ app.use(function(req,res) {
     res.send('Page not found');
 });
 
-//start the server
+//start server
 app.listen(app.get('port'), function() { //listen on port 3000 as assigned above
-    //console.log('Server has been started');  //print a message to user 
 });
 
